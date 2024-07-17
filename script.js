5290b7b12b67747c81423f2df71aef1c12d0e55d
Jepennn <jeppehesselgren@gmail.com>
2024-07-18 00:05:17 +0200

    collected correct data from form

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-//Clear 
+//Clear function, clears all input fields
 function clearAll(){
     
     const inputValues = document.querySelectorAll("input");
@@ -16,6 +16,32 @@ function clearAll(){
     })
 }
 
+function onSubmit(e){
+
+    //avoids to not summit to the file
+    e.preventDefault();
+
+    //Setting upp a easy way to grab data from inputs
+    const formData = new FormData(e.target);
+
+    e.target
+
+    //Grabbing data from inputfields
+    const loanAmount = formData.get('mortageAmount');
+    const loanTimeInYears = formData.get("mortageTerm");
+    const interestRate = formData.get("intrestRate");
+    const mortageType = formData.get("calc-option");
+
+    console.log(loanAmount, loanTimeInYears, interestRate, mortageType);
+
+
+    // Gör din uträkning här sen och pressentera uträkningen i rätt
+    // ställe på skärmen
+}
+
+
+
+
 
 
 
@@ -23,8 +49,10 @@ function clearAll(){
 
 
 const clearAllFields = document.getElementById("clearAll");
+const sumbitForm = document.getElementById("data-form");
 
 
 
 //All eventListener 
 clearAllFields.addEventListener("click", clearAll);
+sumbitForm.addEventListener("submit", onSubmit);
